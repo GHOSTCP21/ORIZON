7f19f68ab54bd3acbcf406fb20ac9414e3770cb5
GHOST <jeancrawforderill@gmail.com>
2026-05-08 23:46:37 -0400

    V.1.1.0

--- Bot/events/ready.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: 'ready',
-  once: true,
-  execute(client) {
-    console.log(`🤖 Connecté en tant que ${client.user.tag}`);
-  }
-};
--- Bot/utils/colors.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  primary: '#1E3A8A',
-  accent: '#E63946',
-  white: '#FFFFFF',
-  cyan: '#00F5FF'
-};
